e9bc506d6b8f6544e1b1fede02bae51df1a1756d
Piepognwong <j.tonneyck@gmail.com>
2020-05-11 14:56:21 +0200

    finished

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,11 +3,15 @@
 
 // ======================
 
-function Rover(initialDirection, initialPosition, manager) {
-  this.direction =  initialDirection;
-  this.position = initialPosition;
-  this.travelLog = [initialPosition]
-  this.turnLeft = function(){
+class Rover {
+  constructor(initialDirection, initialPosition, manager) {
+    this.direction =  initialDirection;
+    this.position = initialPosition;
+    this.travelLog = [initialPosition];
+    this.manager = manager;
+  }
+
+  turnLeft = function(){
     console.log("turnLeft was called!");
     switch(this.direction) {
       case("N"):
@@ -24,7 +28,7 @@ function Rover(initialDirection, initialPosition, manager) {
         break;
     }
   }
-  this.turnRight = function(){
+  turnRight = function(){
     console.log("turnRight was called!");
     switch(this.direction) {
       case("N"):
@@ -41,7 +45,7 @@ function Rover(initialDirection, initialPosition, manager) {
         break;
     }
   }
-  this.moveForward = function(){
+  moveForward = function(){
     console.log("moveForward was called")
     switch(this.direction) {
       case("N"):
@@ -75,10 +79,10 @@ function Rover(initialDirection, initialPosition, manager) {
     }
   
     this.travelLog.push(this.position);
-    manager.checkForObstacleCollision(this); // calling the method of rovermanager
-    manager.checkForRoverCollision(rover1, rover2) // calling the method of rovermanager
+    this.manager.checkForObstacleCollision(this); // calling the method of rovermanager
+    this.manager.checkForRoverCollision(rover1, rover2) // calling the method of rovermanager
   }
-  this.moveBackwards = function(){
+  moveBackwards = function(){
     console.log("move backwards was called")
     switch(this.direction) {
       case("N"):
@@ -111,10 +115,10 @@ function Rover(initialDirection, initialPosition, manager) {
         break;
     }
     this.travelLog.push(this.position);  
-    manager.checkForObstacleCollision(this); // calling the method of rovermanager
-    manager.checkForRoverCollision(rover1, rover2) // calling the method of rovermanager
+    this.manager.checkForObstacleCollision(this); // calling the method of rovermanager
+    this.manager.checkForRoverCollision(rover1, rover2) // calling the method of rovermanager
   }
-  this.instructRover = function(stringOfCommands) {
+  instructRover = function(stringOfCommands) {
     stringOfCommands = stringOfCommands.toLowerCase()
     let commands = "frlb";
     for(var i = 0; i < stringOfCommands.length; i++) {
@@ -139,35 +143,34 @@ function Rover(initialDirection, initialPosition, manager) {
     }
   }
 }
-
-var rover1 = new Rover("N", [4,4])
-var rover2 = new Rover("W", [4,8])
-var rover3 = new Rover("W", [5,8])
-
+  
 /* The rover manager is aware of all the rovers and the obstacles
   an instance of rover manager is also passed to the rovers so that the rovers can
   call the collision detection functions every time they move
 */
-function RoverManager(obstacles){
-  this.rovers = [] //added dynamically through the addRover method
-  this.obstacles = obstacles;
-  this.checkForObstacleCollision = function(rover) {
+class RoverManager{
+  constructor(obstacles){
+    this.obstacles = obstacles;
+  }
+  checkForObstacleCollision = function(rover) {
     for(let i = 0; i<this.obstacles.length; i++) {
       if(this.obstacles[i][0] === rover.position[0] 
         && this.obstacles[i][1] === rover.position[1]) alert("Obstacle BOEM!");
     }
   }
-  this.checkForRoverCollision = function(rover){
+  checkForRoverCollision = function(rover){
     for(let i = 0; i < this.rovers.length; i++) {
       if(this.rovers[i].position[0] === rover.position[0] 
         && this.rovers[i].position[1] === rover.position[1]
         && rover !== this.rovers[i] )alert("Rover BOEM!");
     }
   }
-  this.addRover = function(rover) {
+  rovers = [];
+  addRover = function(rover) {
     this.rovers.push(rover);
   }
 }
+
 var roverManager = new RoverManager([[3,3], [6,7]]);
 var rover1 = new Rover("N", [5,5], roverManager)
 var rover2 = new Rover("N", [9,5], roverManager)
